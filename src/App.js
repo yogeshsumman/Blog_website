3b43ee3ffbee2f9b130e83edb723fe573fd237dc
Yogesh Summan <yogeshsumman2001@gmail.com>
2022-08-08 18:06:36 +0530

    first

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import Navbar from './Components/Navbar';
+import Info from './Components/Info';
+import Footer from './Components/Footer/Footer';
+import img1 from './images/dogo.jpg'
+
+function App() {
+  return (
+    <div>
+      <Navbar/>
+      <Info/>
+      <img src={img1} alt="" id='img1' />
+      <Footer/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Info.js
@@ -0,0 +1,36 @@
+import React from 'react'
+
+export default function Info() {
+return (
+    <>
+<div className="container">
+    <h1 id='heading'>This is my heading</h1>
+    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Impedit, rerum? Dolores quia, adipisci laudantium nulla
+        aliquam magni odio. Magni, temporibus enim quisquam culpa quaerat laborum in facere explicabo incidunt, dolorem
+        illo nihil dicta maxime tempore, eaque ipsam! Dolores natus, harum expedita, ullam ea laboriosam excepturi eum
+        eos autem fugit adipisci! Esse odio neque sint laboriosam a officia! Earum non fugit corporis. Iusto harum quae
+        libero incidunt illum eligendi rem quis sequi, exercitationem reiciendis atque odio voluptates minima qui
+        adipisci amet, assumenda provident delectus. Dignissimos sequi a, est placeat, adipisci iusto tempora molestiae
+        odio ipsa tempore, voluptatum expedita modi voluptatem eveniet reiciendis accusamus mollitia voluptates! Atque
+        ad eligendi odit sapiente perferendis temporibus blanditiis cupiditate incidunt repudiandae saepe, magni
+        quisquam eos, laborum recusandae, praesentium dignissimos soluta animi adipisci dicta autem magnam delectus.
+        Eius, sequi magnam. Ea voluptatum corporis tenetur! Impedit quae consequatur neque ab rem labore nemo quo minus
+        sapiente vero officia voluptatibus expedita voluptatum obcaecati adipisci a cum quam aut, est, qui tempore
+        dolorem provident exercitationem? Unde rerum delectus cumque possimus voluptas deleniti sequi, minus maiores
+        officiis debitis facere aspernatur doloribus dolor voluptates blanditiis eaque eveniet, tenetur alias porro ex
+        dolores! Blanditiis quidem repellendus quasi officia ipsa molestias! Natus omnis accusamus modi aut voluptatibus
+        delectus, aspernatur cupiditate deserunt vel aliquam alias nulla a esse perspiciatis blanditiis eaque quibusdam
+        ipsum voluptate soluta repudiandae quaerat ipsam mollitia odit facere. Veniam ratione quia recusandae debitis
+        magni quam ex repellendus neque sit cum, qui, accusantium voluptatibus quisquam! Molestias corrupti consequatur
+        eaque rerum, voluptatem laborum autem aliquam fugit reiciendis commodi officiis modi minima alias quam
+        asperiores dicta tempora laboriosam, exercitationem recusandae excepturi. Nihil cupiditate culpa porro optio
+        laborum aperiam omnis perspiciatis magni, vel quas quibusdam autem consequatur eum animi commodi tempora
+        consequuntur molestiae eaque consectetur quo debitis quia. Ducimus nostrum voluptatibus quidem iusto
+        consequatur, laboriosam laudantium? </p>
+
+
+
+</div>
+</>
+)
+}
