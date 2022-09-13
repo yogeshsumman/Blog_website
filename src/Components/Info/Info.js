2b2b60b8c62dcf8eb6c0c6621213e6035423eac9
Yogesh Summan <yogeshsumman2001@gmail.com>
2022-09-13 23:23:08 +0530

    changes in the info

--- src/Components/Info/Info.js
+++ src/Components/Info/Info.js
@@ -2,17 +2,26 @@ import React from 'react'
 
 export default function Info() {
 return (
-    <>
-<div className="container">
-    <h1 id='heading'>Welcome to My Blogs</h1>
-    <ul>
-        <li>Latest news</li>
-        <li></li>
-        <li></li>
-        <li></li>
-    </ul>
+<>
+    <div className="container">
+        <h1 id='heading'>Welcome to My Blogs</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis quaerat quos dolorem tempora eveniet
+            suscipit perferendis quisquam error, nihil sit alias eius labore a? Necessitatibus veritatis totam
+            reprehenderit quasi asperiores et quae, doloremque commodi beatae accusantium assumenda laboriosam aliquam
+            voluptates quibusdam quo molestias dicta hic earum? Dolorem obcaecati iure voluptate laudantium. Illum ad
+            corporis fugit? Suscipit pariatur animi nemo adipisci vel impedit voluptate tenetur dicta officiis, illum
+            autem ad fugit beatae ipsum, voluptatibus sequi minus soluta dolorum? Itaque facilis accusamus ipsum iste
+            temporibus! Labore placeat, similique ipsum saepe officiis architecto est itaque quaerat repudiandae,
+            consequuntur eaque incidunt adipisci ipsa in.</p>
+        <ul>
+            <h3>For more information visit</h3>
+            <li><a href="https://www.w3schools.com" target="_blank">Visit W3Schools.com!</a></li>
+            <li><a href="https://www.w3schools.com" target="_blank">Visit W3Schools.com!</a></li>
+            <li><a href="https://www.w3schools.com" target="_blank">Visit W3Schools.com!</a></li>
+            <li><a href="https://www.w3schools.com" target="_blank">Visit W3Schools.com!</a></li>
+        </ul>
 
-</div>
+    </div>
 </>
 )
 }
